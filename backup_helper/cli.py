f1a5b847a729bb1a936ac770707b1e3e26ba256a
omgitsmoe <60219950+omgitsmoe@users.noreply.github.com>
2023-07-08 19:10:09 +0200

    Add logging config

--- backup_helper/cli.py
+++ backup_helper/cli.py
@@ -1,4 +1,7 @@
+import logging
+import logging.config
 import argparse
+import os
 
 from typing import Union
 
@@ -6,6 +9,53 @@ from backup_helper.backup_helper import (
     Source, Target, load_backup_state, load_backup_state_save_always
 )
 from backup_helper.exceptions import SourceNotFound, TargetNotFound
+
+
+def configure_logging(log_path):
+    logging_conf = {
+        'version': 1,
+        'formatters': {
+            'console': {'format': '%(asctime)s - %(levelname)s - %(message)s', 'datefmt': "%H:%M:%S"},
+            "file": {"format": "%(asctime)-15s - %(name)-9s - %(levelname)-6s - %(message)s"}
+        },
+        'handlers': {
+            'console': {
+                'level': 'INFO',
+                'class': 'logging.StreamHandler',
+                'formatter': 'console',
+                'stream': 'ext://sys.stdout'
+            },
+        },
+        'loggers': {
+        },
+        "root": {
+            'level': 'DEBUG',
+            'handlers': ['console']
+        },
+        'disable_existing_loggers': False
+    }
+    if log_path:
+        logging_conf["handlers"].update({
+            'file': {
+                'level': 'DEBUG',
+                'class': 'logging.handlers.RotatingFileHandler',
+                'formatter': 'file',
+                'filename': log_path,
+                'maxBytes': 1048576,
+                'backupCount': 5,
+                "encoding": "UTF-8"
+            }
+        })
+        logging_conf.update(
+            {
+                "root": {
+                    'level': 'DEBUG',
+                    'handlers': ['console', 'file']
+                }
+            }
+        )
+
+    logging.config.dictConfig(logging_conf)
 
 
 def build_parser() -> argparse.ArgumentParser:
@@ -222,9 +272,12 @@ def _cl_modify(args: argparse.Namespace):
 
 
 def main() -> None:
+    configure_logging(None)
     parser = build_parser()
     parsed_args = parser.parse_args()
     if hasattr(parsed_args, 'func') and parsed_args.func:
+        workdir = os.path.dirname(parsed_args.status_file)
+        configure_logging(os.path.join(workdir, 'backup_helper.log'))
         parsed_args.func(parsed_args)
     else:
         parser.print_usage()
